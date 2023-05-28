5f265a1d643af7bab0550808bb52f0fd652d67f4
EvgenPKplace2 <simple_protector@mail.ru>
2023-05-28 17:37:13 +0300

    make hw_2

---
+++ src/lesson_2_hw/Main.java
@@ -0,0 +1,50 @@
+package lesson_2_hw;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[]{
+                1, 4, 56, 5, 4, 7, 8, 56, 2, 3, 5, 34, 65, 87, 23, 45, 67, 898, 23, 90, 8
+        };
+        System.out.println(Arrays.toString(array));
+
+        sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void sort(int[] array) {
+        for (int i = array.length / 2 - 1; i >= 0; i--) {
+            heapify(array, array.length, i);
+        }
+        for (int i = array.length - 1; i >= 0; i--) {
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            heapify(array, i, 0);
+        }
+    }
+
+    private static void heapify(int[] array, int heapSize, int rootIndex) {
+        int largest = rootIndex;
+        int leftChild = 2 * rootIndex + 1;
+        int rightChild = 2 * rootIndex + 2;
+
+        if (leftChild < heapSize && array[leftChild] > array[largest]) {
+            largest = leftChild;
+        }
+
+        if (rightChild < heapSize && array[rightChild] > array[largest]) {
+            largest = rightChild;
+        }
+
+        if (largest != rootIndex) {
+            int temp = array[rootIndex];
+            array[rootIndex] = array[largest];
+            array[largest] = temp;
+
+            heapify(array, heapSize, largest);
+        }
+    }
+}
