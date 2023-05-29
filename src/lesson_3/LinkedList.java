c1d678f02c2e7abfb9a988319c4022b6c69671aa
JohnHozin <simple_protector@mail.ru>
2023-05-29 19:28:09 +0300

    hw_3

---
+++ src/lesson_3/LinkedList.java
@@ -0,0 +1,66 @@
+package lesson_3;
+
+public class LinkedList {
+
+    private Node head;
+
+    private class Node {
+        private int value;
+        private Node next;
+    }
+
+    public void addFirst(int value) {
+        Node first = new Node();
+        first.value = value;
+        if (head != null) {
+            first.next = head;
+        }
+        head = first;
+    }
+
+    public void DeleteFirst() {
+        if (head != null) {
+            head = head.next;
+        }
+    }
+
+    public boolean ifFirst(int value) {
+        Node i = head;
+        while (i != null) {
+            if (i.value == value) {
+                return true;
+            }
+            i = i.next;
+        }
+        return false;
+    }
+
+    public void addLast(int value) {
+        Node last = new Node();
+        last.value = value;
+        if (head == null) {
+            head = last;
+        } else {
+            Node i = head;
+            while (i.next != null) {
+                i = i.next;
+            }
+            i.next = last;
+        }
+    }
+
+    public void deleteLast() {
+        if (head != null) {
+            Node i = head;
+            while (i.next != null) {
+                if (i.next.next == null) {
+                    i.next = null;
+                    return;
+                }
+                i = i.next;
+            }
+            head = null;
+        }
+    }
+}
+
---
+++ src/lesson_3/Main.java
@@ -0,0 +1,30 @@
+package lesson_3;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedListDouble list = new LinkedListDouble();
+        list.addLast(5);
+        list.addLast(6);
+        list.addLast(7);
+        list.addLast(9);
+        list.addLast(1);
+        list.addLast(4);
+        list.printList();
+
+        list.bubbleSort();
+        list.printList();
+
+//        list.deleteFirst();
+//        list.printList();
+//        list.deleteLast();
+//        list.printList();
+
+//        list.revert();
+//        list.printList();
+
+        list.revert();
+        list.printList();
+    }
+
+
+}
